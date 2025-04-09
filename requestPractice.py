bcf20d7bc8f91cb5c404466a4df035f0e4bd49ab
Apurva Anand <apurvanand29@gmail.com>
2025-04-08 22:25:27 -0500

    Added test for put and delete

--- requestPractice.py
+++ requestPractice.py
@@ -37,4 +37,56 @@ for i in range(0, len(list)):
             title = list[i]['title']
 assert title == 'optio molestias id quia eum', f"Actual title does not match expected title '{title}'"
 print(f"Title for id {id} is: {title}")
+
+# Making a POST request
+urlPost = 'https://jsonplaceholder.typicode.com/posts'
+bodyPost = {
+    "title": 'foo',
+    "body": 'bar',
+    "userId": 1
+    }
+headerPost = {'Content-type': 'application/json; charset=UTF-8'}
+response = requests.post(url=urlPost, data=json.dumps(bodyPost), headers=headerPost)
+status = response.status_code
+try:
+    assert status == 201
+    print("✅ Status code is 201 - Test 3 Passed!")
+except AssertionError:
+    print(f"❌ Status code was {status} - Test 3 Failed.")
+
+jsonObject = response.json()
+print(jsonObject['id'])
+print(jsonObject)
+
+# Making a PUT request
+urlPut = 'https://jsonplaceholder.typicode.com/posts/1'
+bodyPut = {
+    "id": 1,
+    "title": 'foot',
+    "body": 'bar',
+    "userId": 1
+}
+headerPut = {'Content-type': 'application/json; charset=UTF-8'}
+response = requests.put(url=urlPut, headers=headerPut, json=jsonObject,)
+status = response.status_code
+try:
+    assert status == 200
+    print("✅ Status code is 200 - Test 4 Passed!")
+except AssertionError:
+    print(f"❌ Status code was {status} - Test 4 Failed.")
+jsonObject = response.json()
+print(jsonObject['id'])
+print(status)
+
+# Making a DELETE request
+urlDelete = 'https://jsonplaceholder.typicode.com/posts/1'
+response = requests.delete(url=urlDelete)
+status = response.status_code
+try:
+    assert status == 200
+    print("✅ Status code is 200 - Test 5 Passed!")
+except AssertionError:
+    print(f"❌ Status code was {status} - Test 5 Failed.")
+print("Deleted successfully")
+
     
