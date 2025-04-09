f86ac0267dc028333a607c8079b7aa04218dc578
Apurva Anand <apurvanand29@gmail.com>
2025-04-08 20:59:45 -0500

    testing for host using GET

--- httpbinPractice.py
+++ httpbinPractice.py
@@ -2,5 +2,11 @@ import requests
 import json
 
 # Making a GET request
-url = 'https://httpbin.org/get'
-response = requests.get(url=url)
+url1 = 'https://httpbin.org/get'
+response = requests.get(url=url1)
+assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
+jsonObject = response.json()
+print(jsonObject['origin'])
+host = jsonObject['headers']['Host']
+expHost = 'httpbin.org'
+assert host == expHost, f"Expected host '{expHost}', but got {host}"
